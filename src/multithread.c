18cb9a2a5ba309b5233fc5d2894bd35a7065cfc2
Oscar Koeroo <okoeroo@gmail.com>
2010-07-02 11:28:56 +0200

    Something still goes wrong... I don't understand. Need to implment proper thread pooling, I think.

--- src/multithread.c
+++ src/multithread.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <signal.h>
+#include <unistd.h>
 #include <pthread.h>
 #include <curl/curl.h>
 
@@ -13,6 +14,10 @@
 #define TESTURL "http://www.nikhef.nl/~okoeroo/testfile"
 #define TESTDATA "foobar2010"
 #define USERAGENT "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.2) Gecko/20100115 Firefox/3.6"
+#define SOMEBYTES 256
+
+/* char * getnextproxy (const char * buffer, char * prev_proxy); */
+char * getnextproxy (const char * buffer);
 
 typedef struct buffer_s
 {
@@ -21,8 +26,14 @@ typedef struct buffer_s
     size_t offset;
 } buffer_t;
 
+typedef struct proxy_list_s {
+    char * proxy;
+    struct proxy_list_s * next;
+} proxy_list_t;
+
 
 pthread_mutex_t output_mutex;
+proxy_list_t * plist;
 
 
 
@@ -44,7 +55,7 @@ static size_t WriteMemoryCallback(void *ptr, size_t size, size_t nmemb, void *da
     size_t realsize = size * nmemb;
     buffer_t *mem = (buffer_t*)data;
 
-    mem->data= myrealloc(mem->data, mem->size + realsize + 1);
+    mem->data = myrealloc(mem->data, mem->size + realsize + 1);
     if (mem->data) {
         memcpy(&(mem->data[mem->size]), ptr, realsize);
         mem->size += realsize;
@@ -57,38 +68,47 @@ static size_t WriteMemoryCallback(void *ptr, size_t size, size_t nmemb, void *da
 static void *pull_one_url(void *proxy)
 {
     CURL *curl;
-    buffer_t chunk;
+    buffer_t * chunk;
     FILE * f = NULL;
 #if 1
-    sigset_t sigset;
+    sigset_t mysigset;
 
-    sigemptyset(&sigset);
-    sigaddset(&sigset, SIGALRM);
-    pthread_sigmask(SIG_BLOCK, &sigset, NULL);
+    sigemptyset(&mysigset);
+    sigaddset(&mysigset, SIGALRM);
+    pthread_sigmask(SIG_BLOCK, &mysigset, NULL);
 #endif
 
-    chunk.data = NULL; /* we expect realloc(NULL, size) to work */ 
-    chunk.size = 0;    /* no data at this point */ 
+    chunk = malloc (sizeof(buffer_t));
+    if (!chunk)
+    {
+        fprintf (stderr, "Failed on creating a chunk!\n");
+        return NULL;
+    }
+
+    chunk -> data = NULL; /* we expect realloc(NULL, size) to work */ 
+    chunk -> size = 0;    /* no data at this point */ 
 
     curl = curl_easy_init();
     curl_easy_setopt(curl, CURLOPT_URL, TESTURL);
     curl_easy_setopt(curl, CURLOPT_USERAGENT, USERAGENT);
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *) &chunk);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *) chunk);
     curl_easy_setopt(curl, CURLOPT_TIMEOUT, TIMEOUT);
 
+#if 1
     if (proxy)
     {
         curl_easy_setopt(curl, CURLOPT_PROXY, proxy);
         curl_easy_setopt(curl, CURLOPT_HTTPPROXYTUNNEL, 1L); 
         /* curl_easy_setopt(easyhandle, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS4);  */
     }
+#endif
 
     curl_easy_perform(curl); /* ignores error */
     curl_easy_cleanup(curl);
 
     /* Test the returned information */
-    if (chunk.data && (strncmp (chunk.data, TESTDATA, strlen(TESTDATA)) == 0))
+    if (chunk -> data && (strncmp (chunk -> data, TESTDATA, strlen(TESTDATA)) == 0))
     {
         pthread_mutex_lock (&output_mutex);
         if ((f = fopen (OUTPUTFILE, "a")))
@@ -99,17 +119,60 @@ static void *pull_one_url(void *proxy)
         pthread_mutex_unlock (&output_mutex);
     }
 
+    if (chunk)
+    {
+        free(chunk -> data);
+        free(chunk);
+        chunk = NULL;
+    }
+
     return NULL;
 }
 
 
-char * getnextproxy (const char * buffer, char * prev_proxy)
+char * getnextproxy (const char * buffer)
 {
-    char * foo = NULL;
-    char * bar = NULL;
-    char * ret = NULL;
     int c = 0;
+    int i = 0;
+    proxy_list_t * cur = NULL;
+    proxy_list_t * loc = plist;
 
+
+    for (i = 0; i < (int)strlen(buffer); i++, c++)
+    {
+        if (!cur)
+        {
+            cur = malloc (sizeof(proxy_list_t));
+            cur -> proxy = malloc(sizeof(char) * SOMEBYTES); /* SOMEBYTES bytes of a proxy address is good enough */
+            memset(cur -> proxy, 0, SOMEBYTES);
+            cur -> next = NULL;
+            c = 0;
+        }
+
+        if ((isalnum(buffer[i])) || (buffer[i] == ':') || (buffer[i] == '.'))
+        {
+            cur -> proxy[c] = buffer[i];
+        }
+        else if (buffer[i] == '\n')
+        {
+            cur -> proxy[c] = '\0';
+            if (!loc)
+            {
+                plist = cur;   /* Add current object to root of list */
+                loc   = cur;
+            }
+            else
+            {
+                loc -> next = cur;
+                loc = loc -> next;
+            }
+            cur   = NULL;
+        }
+    }
+
+    return NULL;
+
+#if 0
     if (prev_proxy)
     {
         foo = strstr(buffer, prev_proxy);
@@ -123,16 +186,15 @@ char * getnextproxy (const char * buffer, char * prev_proxy)
         c = strlen(foo) - strlen(bar);
         ret = malloc (sizeof(char) * (c + 1));
         strncpy (ret, foo, c);
-
         return ret;
     }
     else
         return NULL;
-
-
+#endif
 }
 
-int main(int argc, char **argv)
+/* int main(int argc, char **argv) */
+int main(void)
 {
     pthread_t tid[MAXTHREADS*100];
     int i = 0;
@@ -143,19 +205,21 @@ int main(int argc, char **argv)
     char * proxy = NULL;
     FILE * in = NULL;
     int active_threads = 0;
+    proxy_list_t * loc = NULL;
 
 
     printf ("Settings:\n");
-    printf ("\tthreads:      %d\n", MAXTHREADS);
-    printf ("\ttest URL:     %s\n", TESTURL);
+    printf ("\tThreads:      %d\n", MAXTHREADS);
+    printf ("\tTest URL:     %s\n", TESTURL);
     printf ("\tUser Agent:   %s\n", USERAGENT);
     printf ("\tInput file:   %s\n", INPUTFILE);
     printf ("\tOutput file:  %s\n", OUTPUTFILE);
+    printf ("\tTimeout:      %d\n", TIMEOUT);
     printf ("\n");
 
     pthread_mutex_init (&output_mutex, NULL);
 
-    if (in = fopen (INPUTFILE, "r"))
+    if ((in = fopen (INPUTFILE, "r")))
     {
         fseek(in, 0, SEEK_END);
         bufsize = ftell(in) + 1;
@@ -168,21 +232,46 @@ int main(int argc, char **argv)
         /* Must initialize libcurl before any threads are started */
         curl_global_init(CURL_GLOBAL_ALL);
 
-        /* ugly haxx */
-        while (proxy = getnextproxy(buf, proxy))
+        getnextproxy(buf);
+        loc = plist;
+
+        if (!loc)
         {
+            printf ("No proxies found\n");
+            return 1;
+        }
+        else
+        {
+            while (loc)
+            {
+                printf ("Proxy: %s\n", loc -> proxy);
+                loc = loc -> next;
+            }
+            loc = plist; /* Restore */
+        }
+
+        while (loc)
+        {
+            printf ("%d : %s\n", i, loc -> proxy);
             error = pthread_create(&tid[i],
                     NULL, /* default attributes please */
                     pull_one_url,
-                    (void *)proxy);
+                    (void *)loc -> proxy);
             if(0 != error)
                 fprintf(stderr, "Couldn't run thread number %d, errno %d\n", i, error);
             else
             {
-                fprintf(stderr, "Thread %d, gets via %s\n", i, proxy);
+                fprintf(stderr, "Thread %d, gets via %s\n", i, loc -> proxy);
                 active_threads++;
+                i++;
             }
-            i++;
+            loc = loc -> next;
+#if 0
+            error = pthread_join(tid[i], NULL);
+
+
+            continue;
+#endif
 
             if (active_threads < MAXTHREADS)
                 continue;
@@ -195,6 +284,7 @@ int main(int argc, char **argv)
                     active_threads--;
                     fprintf(stderr, "Thread %d terminated\n", j);
                 }
+                /* i = 0; */
             }
         }
     }
